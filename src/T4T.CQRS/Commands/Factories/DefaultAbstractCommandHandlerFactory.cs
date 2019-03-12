ced110eda1406d7f3a0015d4fad3064203446903
Benjamin Todts <benjamin.todts@t4t.rocks>
2019-03-12 15:53:22 +0100

    Add DefaultAbstractHandlerFactories

---
+++ src/T4T.CQRS/Commands/Factories/DefaultAbstractCommandHandlerFactory.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Autofac.Features.Indexed;
+
+namespace T4T.CQRS.Commands.Factories
+{
+    public class DefaultAbstractCommandHandlerFactory : IAbstractCommandHandlerFactory
+    {
+        private readonly IIndex<string, ICommandHandlerFactory> _concreteFactories;
+        private readonly IComponentContext _componentContext;
+
+        public DefaultAbstractCommandHandlerFactory(IIndex<string, ICommandHandlerFactory> concreteFactories, IComponentContext componentContext)
+        {
+            _concreteFactories = concreteFactories;
+            _componentContext = componentContext;
+        }
+
+        public ICommandHandlerFactory GetFactoryForCommand<T>()
+            where T : class
+        {
+            return _concreteFactories.TryGetValue(typeof(T).Name, out var concreteFactory)
+                ? concreteFactory
+                : new DefaultCommandHandlerFactory(_componentContext);
+        }
+    }
+}
---
+++ src/T4T.CQRS/Queries/Factories/DefaultAbstractQueryHandlerFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Autofac;
+using Autofac.Features.Indexed;
+
+namespace T4T.CQRS.Queries.Factories
+{
+    public class DefaultAbstractQueryHandlerFactory : IAbstractQueryHandlerFactory
+    {
+        private readonly IIndex<Type, IQueryHandlerFactory> _concreteFactories;
+        private readonly IComponentContext _componentContext;
+
+        public DefaultAbstractQueryHandlerFactory(IIndex<Type, IQueryHandlerFactory> concreteFactories,
+            IComponentContext componentContext)
+        {
+            _concreteFactories = concreteFactories;
+            _componentContext = componentContext;
+        }
+
+        public IQueryHandlerFactory GetFactoryForQuery<T>()
+            where T : class
+        {
+            return _concreteFactories.TryGetValue(typeof(T), out var concreteFactory)
+                ? concreteFactory
+                : new DefaultQueryHandlerFactory(_componentContext);
+        }
+    }
+}
