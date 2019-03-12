50a90d010f18f360ba13310babacfed5737d99a6
Benjamin Todts <benjamin.todts@t4t.rocks>
2019-03-12 15:40:55 +0100

    Add ApiControllerBase

---
+++ src/T4T.CQRS/Api/ApiControllerBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using T4T.CQRS.Commands.Factories;
+using T4T.CQRS.Execution;
+using T4T.CQRS.Queries.Factories;
+
+namespace T4T.CQRS.Api
+{
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        private readonly IAbstractCommandHandlerFactory _abstractCommandHandlerFactory;
+        private readonly IAbstractQueryHandlerFactory _abstractQueryHandlerFactory;
+
+        protected ApiControllerBase(IAbstractCommandHandlerFactory abstractCommandHandlerFactory, IAbstractQueryHandlerFactory abstractQueryHandlerFactory)
+        {
+            _abstractCommandHandlerFactory = abstractCommandHandlerFactory;
+            _abstractQueryHandlerFactory = abstractQueryHandlerFactory;
+        }
+
+        protected async Task<ExecutionResult> HandleCommand<T>(T command,
+            CancellationToken cancellationToken = default(CancellationToken))
+            where T : class
+        {
+            var factory = _abstractCommandHandlerFactory.GetFactoryForCommand<T>();
+            var commandHandler = factory.CreateCommandHandler<T>();
+            return await commandHandler.Handle(command, cancellationToken);
+        }
+
+        protected async Task<TResult> HandleQuery<TQuery, TResult>(TQuery query,
+            CancellationToken cancellationToken = default(CancellationToken))
+            where TQuery : class
+            where TResult : ExecutionResult
+        {
+            var factory = _abstractQueryHandlerFactory.GetFactoryForQuery<TQuery>();
+            var queryHandler = factory.CreateQueryHandler<TQuery, TResult>();
+            return await queryHandler.Handle(query, cancellationToken);
+        }
+    }
+}
