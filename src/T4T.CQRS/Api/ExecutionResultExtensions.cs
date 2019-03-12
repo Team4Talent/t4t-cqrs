62d6cbe6bff72a6b044b1013b896128e32b2a5e3
Benjamin Todts <benjamin.todts@t4t.rocks>
2019-03-12 16:01:26 +0100

    Add ExecutionResultExtensions

---
+++ src/T4T.CQRS/Api/ExecutionResultExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using T4T.CQRS.Execution;
+
+namespace T4T.CQRS.Api
+{
+    public static class ExecutionResultExtensions
+    {
+        public static IActionResult ToActionResult<TResult>(this TResult queryResult)
+            where TResult : ExecutionResult
+        {
+            // Check errors by order of severity.
+            if (queryResult.Errors.Any(e => e.Type == ExecutionErrorType.InternalServerError))
+                return new ObjectResult(queryResult) {StatusCode = (int) HttpStatusCode.InternalServerError};
+
+            if (queryResult.Errors.Any(e => e.Type == ExecutionErrorType.BadRequest))
+                return new ObjectResult(queryResult) {StatusCode = (int) HttpStatusCode.BadRequest};
+
+            if (queryResult.Errors.Any(e => e.Type == ExecutionErrorType.NotFound))
+                return new NotFoundObjectResult(queryResult) {StatusCode = (int) HttpStatusCode.NotFound};
+
+            if (queryResult.Errors.Any(e => e.Type == ExecutionErrorType.Forbidden))
+                return new ObjectResult(queryResult) {StatusCode = (int) HttpStatusCode.Forbidden};
+
+            return new OkObjectResult(queryResult);
+        }
+
+        public static IActionResult ToCreatedResult(this ExecutionResult commandResult, string location, string id)
+        {
+            var result = commandResult.ToActionResult();
+
+            return !(result is OkObjectResult)
+                ? result
+                : new CreatedResult(location,
+                    new {id, commandResult.Errors, commandResult.Warnings, commandResult.Success});
+        }
+    }
+}
