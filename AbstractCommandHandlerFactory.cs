798d48ede056b1256b824bf6cec2fee1397144b7
Benjamin Todts <Benjamin.Todts@mediahuis.be>
2021-01-07 21:54:53 +0100

    Add recipes for Autofac.

---
+++ AbstractCommandHandlerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Autofac;
+using Autofac.Features.Indexed;
+
+public class AbstractCommandHandlerFactory : IAbstractCommandHandlerFactory
+{
+    private readonly IIndex<Type, ICommandHandlerFactory> _concreteFactories;
+    private readonly IComponentContext _componentContext;
+
+    public AbstractCommandHandlerFactory(IIndex<Type, ICommandHandlerFactory> concreteFactories, IComponentContext componentContext)
+    {
+        _concreteFactories = concreteFactories;
+        _componentContext = componentContext;
+    }
+
+    public ICommandHandlerFactory GetFactoryForCommand<T>()
+        where T : class
+    {
+        // See if a concrete registration exists for type T. If it doesn't, return a default CommandHandlerFactory.
+        // This enables switching out factories for given CommandHandlers without touching the consuming controllers/classes/...
+        return _concreteFactories.TryGetValue(typeof(T), out var concreteFactory)
+            ? concreteFactory
+            : new CommandHandlerFactory(_componentContext);
+    }
+}
---
+++ CommandHandlerFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Autofac;
+using Microsoft.Extensions.Logging;
+using T4T.CQRS.Extensions;
+
+public class CommandHandlerFactory : ICommandHandlerFactory
+{
+    private readonly IComponentContext _container;
+
+    public CommandHandlerFactory(IComponentContext container)
+    {
+        _container = container;
+    }
+
+    // An example of a "default" CommandHandlerFactory that simply searches Autofac for a specific handler
+    // registered for type T and wraps it with some logging and exception handling.
+    public ICommandHandler<T> CreateCommandHandler<T>() 
+        where T : class
+    {
+        var commandHandler = _container.Resolve<ICommandHandler<T>>() ?? 
+                             throw new ArgumentException($"Could not resolve an ICommandHandler for {typeof(T).Name}.", innerException: null);
+        var loggerFactory = _container.Resolve<ILoggerFactory>();
+
+        return new ExceptionHandlingCommandHandler<T>(commandHandler)
+            .WithLogging(loggerFactory, LogLevel.Warning);
+    }
+}
+
